ead17e17d3682fad0c00bdd69e5cee3ecbe5fd8d
Sam (Sangho Kim) <rlaace423@gmail.com>
2018-09-19 16:52:44 +0900

    updated log.sh

--- log.sh
+++ log.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
+CONFIGURATION_SHELL_NAME=$(pwd)"/configuration_info.sh"
+
+if [ ! -f $CONFIGURATION_SHELL_NAME ]; then
+    printf "Cannot find %s\n" $CONFIGURATION_SHELL_NAME
+    exit 1;
+fi
+
+. $CONFIGURATION_SHELL_NAME
+
+if [ ! -d "$NODEOS_DATA_DIR" ]; then
+    printf "Creating nodeos/data directory...\n"
+    mkdir "$NODEOS_DATA_DIR"
+fi
+
+if [ ! -d "$NODEOS_LOG_DIR" ]; then
+    printf "Creating nodeos/log directory...\n"
+    mkdir "$NODEOS_LOG_DIR"
+fi
 
 default_line_fetch=50
-target_dir="$(pwd)/nodeos/log"
 
-last=$(ls -Ft "$target_dir" | grep -v '[/=|]$' | head -1)
-
-echo ${#last}
+last=$(ls -Ft "$NODEOS_LOG_DIR" | grep -v '[/=|]$' | head -1)
 
 if [ ${#last} -eq 0 ]; then
-    echo "No file!"
+    echo "No file exists!"
     exit
 fi
 
-echo "$target_dir/$last"
-
-tail -f -n $default_line_fetch "$target_dir/$last"
+echo " - Target file : $NODEOS_LOG_DIR/$last"
+echo " - from last $default_line_fetch lines ... "
+echo
+tail -f -n $default_line_fetch "$NODEOS_LOG_DIR/$last"
